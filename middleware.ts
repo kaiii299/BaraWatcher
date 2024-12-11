df743a4cb890aec08be38947b4d2b7240bb6f957
kaiii299 <151477267+MuuseTechSg@users.noreply.github.com>
2024-12-11 16:00:24 +0800

    changes middware

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,22 @@ import type { Session } from "better-auth/types";
 import { NextResponse, type NextRequest } from "next/server";
 
 export default async function authMiddleware(request: NextRequest) {
-
   const baseURL = process.env.NEXT_PUBLIC_BASE_URL || request.nextUrl.origin;
 
-  const { data: session } = await betterFetch<Session>(
+  const { data: session, error } = await betterFetch<Session>(
     "/api/auth/get-session",
     {
-      baseURL: baseURL ,
+      baseURL: baseURL,
       headers: {
-        //get the cookie from the request
         cookie: request.headers.get("cookie") || "",
       },
     },
   );
+
+  if (error) {
+    console.error("Failed to fetch session:", error);
+    return NextResponse.next(); // Handle error appropriately
+  }
 
   const path = request.nextUrl.pathname;
   const isAuthPage = path === "/login" || path === "/register";
